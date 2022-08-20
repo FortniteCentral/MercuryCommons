300cc38184300519663a5fac0501cf44ce85a7b7
GMatrixGames <gmatrixgames@hotmail.com>
2022-08-20 13:32:13 -0400

    platform

---
+++ MercuryCommons/Framework/Fortnite/API/Enums/EpicPlatform.cs
@@ -0,0 +1,24 @@
+﻿namespace MercuryCommons.Framework.Fortnite.API.Enums;
+
+public enum EpicPlatform
+{
+    Windows,
+    Mac,
+    IOS,
+    Android
+}
+
+public static class PlatformExtension
+{
+    public static string GetForwardFacingName(this EpicPlatform platform)
+    {
+        return platform switch
+        {
+            EpicPlatform.Windows => "Windows",
+            EpicPlatform.Mac => "MacOS",
+            EpicPlatform.IOS => "iOS",
+            EpicPlatform.Android => "Android",
+            _ => "Windows"
+        };
+    }
+}
