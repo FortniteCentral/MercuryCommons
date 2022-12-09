24a359898c1a2380502dd17af4e5c51216a21616
GMatrixGames <gmatrixgames@hotmail.com>
2022-12-09 15:39:55 -0500

    uri combine

--- MercuryCommons/Utilities/Extensions/UriExtensions.cs
+++ MercuryCommons/Utilities/Extensions/UriExtensions.cs
@@ -1,11 +1,16 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
 using System.Net.Http;
 
 namespace MercuryCommons.Utilities.Extensions;
 
 public static class UriExtensions
 {
+    public static Uri Combine(this Uri uri, params string[] paths) => new(paths.Aggregate(uri.AbsoluteUri, (current, path) => $"{current.TrimEnd('/')}/{path.TrimStart('/')}"));
+
+    public static string CombineToString(this string uri, params string[] paths) => new Uri(paths.Aggregate(uri, (current, path) => $"{current.TrimEnd('/')}/{path.TrimStart('/')}")).AbsoluteUri;
+
     public static bool TryGetBytes(this Uri link, out byte[] data)
     {
         data = new HttpClient().GetByteArrayAsync(link).GetAwaiter().GetResult();
