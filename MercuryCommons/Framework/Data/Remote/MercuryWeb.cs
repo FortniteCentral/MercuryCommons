a6a8b8f6c46bbc451ef9d5dce99e0ff709535bca
GMatrixGames <gmatrixgames@hotmail.com>
2022-12-11 08:14:26 -0500

    execute for response

--- MercuryCommons/Framework/Data/Remote/MercuryWeb.cs
+++ MercuryCommons/Framework/Data/Remote/MercuryWeb.cs
@@ -1,6 +1,5 @@
 ﻿using System.Collections.Generic;
 using System.IO;
-using System.Net.Http;
 using System.Reflection;
 using System.Threading.Tasks;
 using RestSharp;
@@ -32,6 +31,7 @@ public class MercuryWeb
     /// <param name="parameters">Query parameters to include in the request</param>
     /// <param name="headers">Headers to include in the request</param>
     /// <param name="files">Files to include in the request</param>
+    /// <param name="body">Json body to include in the request</param>
     /// <param name="method">HTTP method to use</param>
     /// <typeparam name="T">JSON model to use in return value</typeparam>
     /// <returns>JSON model created from json</returns>
@@ -74,13 +74,58 @@ public class MercuryWeb
     }
 
     /// <summary>
+    /// Execute request to get a full response
+    /// </summary>
+    /// <param name="url">Url to request from</param>
+    /// <param name="parameters">Query parameters to include in the request</param>
+    /// <param name="headers">Headers to include in the request</param>
+    /// <param name="files">Files to include in the request</param>
+    /// <param name="body">Json body to include in the request</param>
+    /// <param name="method">HTTP method to use</param>
+    /// <returns>JSON model created from json</returns>
+    public static async Task<RestResponse> ExecuteForResponse(string url, Dictionary<string, object> parameters = null, Dictionary<string, object> headers = null, Dictionary<string, byte[]> files = null, object body = null, Method method = Method.Get)
+    {
+        var request = new MercuryRequest(url, method);
+        
+        if (parameters is { Count: > 0 })
+        {
+            foreach (var (key, value) in parameters)
+            {
+                request.AddParameter(key, value, ParameterType.QueryString);
+            }
+        }
+
+        if (headers is { Count: > 0 })
+        {
+            foreach (var (key, value) in headers)
+            {
+                request.AddHeader(key, value.ToString() ?? string.Empty);
+            }
+        }
+
+        if (files is { Count: > 0 })
+        {
+            foreach (var (key, value) in files)
+            {
+                request.AddFile(key, value, key);
+            }
+        }
+
+        if (body != null) request.AddJsonBody(body);
+        
+        var response = await Client.ExecuteAsync(request).ConfigureAwait(false);
+        Log.Information("[{Method}] [{Status}({StatusCode})] '{Resource}'", request.Method, response.StatusDescription ?? "null", (int) response.StatusCode, response.ResponseUri?.OriginalString ?? "null");
+        return response;
+    }
+
+    /// <summary>
     /// Download a url to a file. Should only be used with urls where the filetype is known.
     /// </summary>
     /// <param name="url">Url of site</param>
     /// <param name="path">Local file path to download to, including extension</param>
     public static async Task DownloadFileAsync(string url, string path)
     {
-        var data = GetByteArray(url);
+        var data = await GetByteArrayAsync(url);
         await File.WriteAllBytesAsync(path, data);
     }
 
@@ -91,7 +136,7 @@ public class MercuryWeb
     /// <returns>Data of site, null if not successful</returns>
     public static byte[] GetByteArray(string url)
     {
-        var request = new RestRequest(url);
+        var request = new MercuryRequest(url);
         var data = Client.Execute(request);
         byte[] retData = null;
         if (data.IsSuccessful) retData = Client.DownloadData(request);
@@ -105,7 +150,7 @@ public class MercuryWeb
     /// <returns>Data of site, null if not successful</returns>
     public static async Task<byte[]> GetByteArrayAsync(string url)
     {
-        var request = new RestRequest(url);
+        var request = new MercuryRequest(url);
         var data = await Client.ExecuteAsync(request);
         byte[] retData = null;
         if (data.IsSuccessful) retData = await Client.DownloadDataAsync(request);
