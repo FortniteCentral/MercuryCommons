e91199101bf55ae78fcbf0c1ec6e16eb1ede3184
GMatrixGames <gmatrixgames@hotmail.com>
2023-03-25 17:14:46 -0400

    Fix

--- MercuryCommons/Framework/Fortnite/API/Services/LauncherPublicService.cs
+++ MercuryCommons/Framework/Fortnite/API/Services/LauncherPublicService.cs
@@ -1,5 +1,6 @@
 ﻿using System.Threading.Tasks;
 using MercuryCommons.Framework.Fortnite.API.Enums;
+using MercuryCommons.Framework.Fortnite.API.Objects.Auth;
 using MercuryCommons.Framework.Unreal.Manifests.Objects;
 using Newtonsoft.Json;
 using RestSharp;
@@ -25,14 +26,15 @@ public class LauncherPublicService : BaseService
     /// <param name="namespace">EGS Namespace of game</param>
     /// <param name="platform">Platform of game</param>
     /// <param name="label">Version label of game</param>
+    /// <param name="auth">Auth response for entitlements</param>
     /// <returns>Manifest info</returns>
-    public async Task<ManifestInfo> GetGameManifestAsync(string appId, string catalogId, string @namespace, string platform = "Windows", string label = "Live")
+    public async Task<ManifestInfo> GetGameManifestAsync(string appId, string catalogId, string @namespace, string platform = "Windows", string label = "Live", AuthResponse auth = null)
     {
-        var launcher = await Client.AccountPublicService.GetAccessTokenAsync(GrantType.ClientCredentials, ClientToken.LauncherAppClient2);
-        if (!launcher.IsSuccessful) return null;
+        var authResponse = auth;
+        if (authResponse == null) authResponse = (await Client.AccountPublicService.GetAccessTokenAsync(GrantType.ClientCredentials, ClientToken.LauncherAppClient2)).Data;
         var request = new RestRequest($"/launcher/api/public/assets/v2/platform/{platform}/namespace/{@namespace}/catalogItem/{catalogId}/app/{appId}/label/{label}", Method.Post);
         if (@namespace == "fn" && platform == "Android" && catalogId == "4fe75bbc5a674f4f9b356b5c90567da5") request.AddJsonBody(new { abis = new[] { "arm64-v8a" } }); // Manual
-        var response = await ExecuteAsync<object>(request, true, accessToken: launcher.Data.AccessToken, requiresLogin: false);
+        var response = await ExecuteAsync<object>(request, true, accessToken: authResponse.AccessToken, requiresLogin: false);
         return response.IsSuccessful && response.Data != null ? new ManifestInfo(JsonConvert.SerializeObject(response.Data)) : null;
     }
 
